e079e5f60be494e6756de7037c9d9e660ee5c27c
Your Name <abudawud.adi@gmail.com>
2023-05-12 08:47:41 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const init = async () => {
         port: 5000,
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
-        cors: {
-        origin: ['*'],
+            cors: {
+                origin: ['*'],
         },
         },
     });
